c09fec9164f33e85a46201ac083673d3454ad8d3
Loyoenock <loyoenock@gmail.com>
2024-05-21 11:11:19 +0300

    Initial Project setup and Hero section

---
+++ app/layout.tsx
@@ -0,0 +1,31 @@
+import { Inter } from "next/font/google";
+import "./globals.css";
+import { ThemeProvider } from "next-themes";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata = {
+  title: "Enoch's Portfolio",
+  description: "Showcases my Projects",
+};
+
+export default function RootLayout({
+   children,
+   }: Readonly<{
+     children: React.ReactNode;
+     }>) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>
+      <ThemeProvider
+            attribute="class"
+            defaultTheme="dark"
+            enableSystem
+            disableTransitionOnChange
+          >
+            {children}
+          </ThemeProvider>
+      </body>
+    </html>
+  );
+}
