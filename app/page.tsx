06e322fd8c5b6da08a25fefe707fb38414772183
Loyoenock <loyoenock@gmail.com>
2024-05-22 22:33:49 +0300

    Component - Footer section

--- app/page.tsx
+++ app/page.tsx
@@ -7,10 +7,12 @@ import Grid from "@/components/Grid"
 import RecentProjects from "@/components/RecentProjects";
 import Clients from "@/components/Clients";
 import Experience from "@/components/Experience";
+import Approach from "@/components/Approach";
+import Footer from "@/components/Footer";
 
 const Home = () => {
   return (
-    <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
+    <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-clip mx-auto sm:px-10 px-5">
       <div className="max-w-7xl w-full">
         <FloatingNav navItems={navItems} />
         <Hero />
@@ -18,6 +20,8 @@ const Home = () => {
         <RecentProjects />
         <Clients />
         <Experience />
+        <Approach />
+        <Footer />
       </div>
     </main>
   );
